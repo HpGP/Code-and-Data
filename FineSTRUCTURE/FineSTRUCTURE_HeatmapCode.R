24378661936dc41b3c01fac8f60887cac888bdce
Zilia Y. Muñoz Ramirez <zilia.murmz@gmail.com>
2023-05-24 21:51:36 -0600

    Update FineSTRUCTURE_HeatmapCode.R

--- FineSTRUCTURE/FineSTRUCTURE_HeatmapCode.R
+++ FineSTRUCTURE/FineSTRUCTURE_HeatmapCode.R
@@ -62,55 +62,37 @@ mapstatematrix<-groupingAsMatrix(mapstatelist)[fullorder,fullorder] # map state 
 
 # Coancestry matrix
 datamatrix<-dataraw[fullorder,fullorder] 
-datamatrix
-
-ids_list <-read.csv (file="ids_colours_list.csv",header=FALSE,sep = ",",check.names = FALSE)
-ids_list
-
-colnames(ids_list)
-
-ids_list %>% 
-  mutate(alias = deframe(df2)[cliente])
 
 # Save coancestry matrix with IND ids
 write.csv(datamatrix, "datamatrix.csv")
 
-###### HACER CAMBIO DE IDS DE FORMA MANUAL
-#leer matrix
+# Note
+# Replace the INDXXX IDs with the IDs of each strain according to the list_colour_2022.csv file, 
+# using an external program (such as Excel using the VLOOKUP function).
+
+#Read the matrix with ids names
 InputFileData <- "datamatrix_R.csv"
+datamatrix_R <-read.table(file=InputFileData,header=TRUE,sep = ",",check.names = FALSE)
 
-df <-read.table(file=InputFileData,header=TRUE,sep = ",",check.names = FALSE)
-row.names(df)
-
-#names_pop <- df$X
-names_pop <- df[,1]
-df[,1] <- NULL
-#df$X <- NULL
-rownames(df) = names_pop
-df2 <- as.matrix(df)
+names_pop <- datamatrix_R[,1]
+datamatrix_R[,1] <- NULL
+rownames(datamatrix_R) = names_pop
+datamatrix_R <- as.matrix(datamatrix_R)
 
 # make some colours
 some.colors<-MakeColorYRP() # these are yellow-red-purple
 some.colorsEnd<-MakeColorYRP(final=c(0.2,0.2,0.2)) # as above, but with a dark grey final for capped values
 
 tmatmax<-200 # cap the heatmap
-tmpmat<-df2 
+tmpmat<-datamatrix_R 
 tmpmat[tmpmat>tmatmax]<-tmatmax  
 
-
-#el color de los labels de col
-cols <- rep('#828282', ncol(df2))
-
-#el color de los labels de row
-cols_rows <- rep('#828282', nrow(df2))
-
-
-#---- Colores ----
-list_color <-read.table(file="list_colour_2022.csv",header=TRUE,sep = ",")
-list_color
+#Colours
+cols <- rep('#828282', ncol(datamatrix_R))
+cols_rows <- rep('#828282', nrow(datamatrix_R))
+list_color <-read.table(file="list_colour.csv",header=TRUE,sep = ",")
 
 colours_list_Pop <- rep('black', nrow(list_color))
-colours_list_Pop
 colours_list_Pop[list_color$POP %in% "HpAfrica2"] <- '#000000'
 colours_list_Pop[list_color$POP %in% "hspAfrica1SAfrica"] <- '#D9D9D9' 
 colours_list_Pop[list_color$POP %in% "hpNEAfrica"] <- '#008041' 
@@ -129,10 +111,6 @@ colours_list_Pop[list_color$POP %in% "hpNorthAsia"] <- '#EB3D4F'
 colours_list_Pop[list_color$POP %in% "hspIndigenous"] <- '#7B4BA0'
 colours_list_Pop[list_color$POP %in% "hspEAsia"] <- '#C93342'
 
-#---- colours_list_Pop (DAPC) ----
-list_color <-read.table(file="list_colour_2022.csv",header=TRUE,sep = ",")#(file="list_color_1020Hp.csv",header=TRUE,sep = ",")
-list_color
-
 colours_list_DAPC <- rep('black', nrow(list_color))
 colours_list_DAPC
 colours_list_DAPC[list_color$DAPC %in% "1"] <- '#008041' 
@@ -142,180 +120,33 @@ colours_list_DAPC[list_color$DAPC %in% "4"] <- '#000000'
 colours_list_DAPC[list_color$DAPC %in% "5"] <- '#EB3D4F' 
 colours_list_DAPC[list_color$DAPC %in% "6"] <- '#D5B7ED' 
 
-rlab=as.matrix(t(cbind(colours_list_Pop,colours_list_DAPC)))
+rlab=as.matrix(t(cbind(colours_list_Pop)))
 clab=as.matrix((cbind(colours_list_Pop,colours_list_DAPC)))
 
+#Making heatmap
+pdf("Fs_coancestry_heatmap.pdf",height=48,width=48)
 
-pdf("Draft005.pdf",height=48,width=48)
+library(downloader)
+source_url("https://raw.githubusercontent.com/obigriffith/biostar-tutorials/master/Heatmaps/heatmap.3.R")
 
-p<-heatmap.3( tmpmat, #la funcion que se utiliznado usualmente es heatmap.2
-             scale = "none", #scale data per row, column, or none
-             Rowv=tdend, #nota importante tdend se obtiene corriendo primero el codigoR_Hp.r de fineSTRUCTURE
+p<-heatmap.3( tmpmat, 
+             scale = "none",
+             Rowv=tdend,
              Colv=tdend, 
-             dendrogram = "both", #"both","row","column","none"
+             dendrogram = "both", 
              trace="none",
              key=TRUE,
              keysize = 0.5,
-             cexRow=0.3, #0.1 
-             cexCol = 0.3, #0.1
+             cexRow=0.3,  
+             cexCol = 0.3, 
              col=some.colorsEnd, #dendrogram color palette
-             #symkey=FALSE,
-             #xlab = "Identifier",
-             #ylab = "Rows",
-             #colCol = cols,
+             colCol = cols,
+             colRow = cols_rows,
              RowSideColorsSize=2,
              ColSideColorsSize=2,
-             ColSideColors=clab,#colours_list_Pop,
-             RowSideColors=rlab#colours_list_Pop#colours_list_cagPAI, #colours_list_Fs#colours_list_ISMEJ # 
-             #colCol = cols,
-             #colRow = cols_rows,
-             #ColSideColors=colours_list,
-             #lmat=rbind(c(0,5,4,0,0), c(0,3,2,1,0)), 
-             #lhei=c(2,5),
-             #lwid=c(1,1,4,0.25,1)
+             ColSideColors=clab,
+             RowSideColors=rlab
+             
              )
 
-
 dev.off()
-
-
-#---- Dendogram figure ----
-library(dendextend)
-IDS <- list_color$ID_FILES
-IDS <- list_color$Strain_name
-pdf(file="Hp_FullDendrogram_Colour_Version21Nov2022.pdf",height=8,width=150) #24 normal orginal
-par(mar=c(6,0,2,0),mfrow=c(1,1))
-tdend %>% set("labels", IDS ) %>%  set("labels_col", colours_list_Pop) %>% set("labels_cex", 1) %>% plot #Colour
-
-tdend %>% set("labels", IDS)  %>% set("labels_cex", 0.5) %>% plot #RAW
-
-#tdend %>% rect.dendrogram(k=24, border = 8, lty = 5, lwd = 1)
-tdend %>% rect.dendrogram(k=18, border = 8, lty = 5, lwd = 1)
-#tdend %>% rect.dendrogram(k=18, border = 8, lty = 5, lwd = 1)
-#tdend %>% rect.dendrogram(k=13, border = 8, lty = 5, lwd = 1) ##This 
-
-abline(h = 9, lty = 2,col="orange",lwd = 2) #18
-abline(h = 10, lty = 2,col="red",lwd = 2) #13
-abline(h = 11, lty = 2,col="blue",lwd = 2) #23
-
-#the_bars =cbind(DAPC=colours_list,ISMEJ=colours_list_ISMEJ,new=colours_list_Fs,cagPAI=colours_list_cagPAI)
-the_bars =cbind(#Level10_K13=colours_list_Group13,
-                #subregion=colours_list_subregion, #NCBI
-                #Level11_K18=colours_list_k18, #NCBI
-                #Level9_K18=colours_list_Group18,
-                #Level8_K23=colours_list_Group23, 
-                #Level7_K24=colours_list_Group24, 
-                #Level6_K31=colours_list_Group31, 
-                #Previous=colours_list_Pop,
-                Pop=colours_list_Pop)
-
-colored_bars(colors = the_bars, dend = tdend)
-
-dev.off()
-
-#---- PCA Principal Components Analysis ----
-data_dataraw <-data.frame(dataraw)#####extra
-write.csv(data_dataraw, file = "data_dataraw.csv")#####extra
-
-
-pcares<-mypca(dataraw)
-pcares$values
-
-saveRDS(pcares, file="pcares.RData")
-
-# For figuring out how many PCs are important; see Lawson & Falush 2012
-# You need packages GPArotation and paran
-tmap<-optimalMap(dataraw)
-warnings()
-thorn<-optimalHorn(dataraw)
-thorn
-c(tmap,thorn) # 11 and 5. Horn typically underestimates, Map is usually better
-pcapops<-getPopIndices(rownames(dataraw),mapstatelist)
-pcapops
-data_pcapops <-data.frame(pcapops)#####extra
-write.csv(data_pcapops, file = "data_pcapops.csv")#####extra
-
-pcanames<-rownames(dataraw)
-pcanames
-
-write.csv(pcanames, file = "data_pcanames.csv")#####extra
-
-rcols<-rainbow(max(pcapops))
-pcapops
-
-data_rcols<-data.frame(rcols) #####extra
-write.csv(data_rcols, file = "data_rcols.csv")#####extra
-
-
-que <-data.frame(rcols[pcapops])#####extra
-Colors_pop_file = read.delim("Colors.txt", header=FALSE)#####extra
-
-Colors_pop <- Colors_pop_file$V1 #####extra
-
-###PCANAMES
-pcanames_file = read.csv("data_pcanames3.csv")#####extra
-pcanames2 <- pcanames_file$x #####extra
-
-class(pcanames)
-pcanames
-class(pcanames2)
-pcanames2<-as.character(pcanames2)
-class(pcanames2)
-pcanames2
-
-###PCANAMES_NOW
-pcanames_file_NOW = read.csv("data_pcanames2_NOW.csv")#####extra
-pcanames2_NOW <- pcanames_file_NOW$x #####extra
-
-class(pcanames2_NOW)
-pcanames2_NOW<-as.numeric(pcanames2_NOW)
-class(pcanames2_NOW)
-
-
-
-###RCOLS
-file_rcols2 = read.csv("rcols_2020.csv")#####extra
-#file_rcols2[,1] <- NULL #####extra
-rcols2 <- file_rcols2$rcols #####extra
-
-class(rcols)
-rcols
-class(rcols2)
-rcols2
-rcols2<-as.character(rcols2)
-class(rcols2)
-
-###PCAPOPS
-
-#pcapops2 = read.csv("data_pcapops_IDS.csv") #####extra
-#names <- as.character(pcapops2[,1]) #####extra
-#row.names(pcapops2)<- names #####extra
-#pcapops2[,1] <- NULL #####extra
-#pcapops2 <- unclass(pcapops2) #####extra
-
-
-
-pcares$vectors[,i]
-
-pdf("PCA.pdf",height=48,width=36)
-par(mfrow=c(4,3))
-for(i in 1:10) for(j in (i+1):11) {
-  plot(pcares$vectors[,i],
-       pcares$vectors[,j],
-       #col=rcols[pcapops], #original
-       #col=rcols2[pcapops], #####extra
-       col=Colors_pop, #####extra
-       xlab=paste("PC",i),
-       ylab=paste("PC",j),
-       main=paste("PC",i,"vs",j),
-       # pch = c(16, 17, 18)[as.numeric(Species)]
-       pch=16, #orignal
-  )
-      # pch= pcanames2_NOW )
-  #text(pcares$vectors[,i],pcares$vectors[,j],labels=pcanames,col=rcols[pcapops],cex=0.5,pos=1) #original
-  text(pcares$vectors[,i],pcares$vectors[,j],labels=pcanames2,cex=0.5,pos=1) #####extra
-}
-dev.off()
-
-
-
